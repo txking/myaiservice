9ea3b746cc89ccd23b5914176e3f89053e553bb9
txking <g_rajesh_98@yahoo.com>
2017-08-23 18:46:27 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -53,8 +53,8 @@ def makeWebhookResult(req):
         zohourl = "https://creator.zoho.com/api/json/loaninfo/view/values_Report?authtoken=e22a32c53525cdf5af604978a70a5c4a&zc_ownername=grajesh2000&criteria=(LoanNo%20%3D%3D%20" + loanno + ")"
         print ('LoanNo is {0}, and URL is {1}'.format(loanno, zohourl))
         sres = loanno
-        # sres = urllib.request.urlopen(zohourl).read()
-        speech = sres
+        sres = urllib.request.urlopen(zohourl).read()
+        speech = sres.decode("utf-8")
         print("Response:")
         print(speech)
         return {
