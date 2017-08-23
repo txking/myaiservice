63de50051c64982f8b664c391be78222f452b723
txking <g_rajesh_98@yahoo.com>
2017-08-23 15:01:47 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -29,33 +29,28 @@ def webhook():
 
 def makeWebhookResult(req):
     if req.get("result").get("action") == "shipping.cost":
-	result = req.get("result")
-	parameters = result.get("parameters")
-	zone = parameters.get("shipping-zone")
-
-	cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
-
-	speech = "The APPROXIMATE cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
-
-	print("Response:")
-	print(speech)
-
-	return {
-	"speech": speech,
-	"displayText": speech,
-	#"data": {},
-	# "contextOut": [],
-	"source": "apiai-onlinestore-shipping"
-	}
-	
+        result = req.get("result")
+        parameters = result.get("parameters")
+        zone = parameters.get("shipping-zone")
+        cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+        speech = "The APPROXIMATE cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+        print("Response:")
+        print(speech)
+        return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-onlinestore-shipping"
+        }
     else:
-	return {}
+        return {}
 	
 
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
-    # print "Starting app on port %d" % port
+    print "Starting app on port %d" % port
 
     app.run(debug=True, port=port, host='0.0.0.0')
