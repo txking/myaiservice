3032cc114b9dab4748f07c0e5f30a74ee70ca6d3
txking <g_rajesh_98@yahoo.com>
2017-08-23 14:49:23 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -22,37 +22,40 @@ def webhook():
     res = makeWebhookResult(req)
 
     res = json.dumps(res, indent=4)
-    print(res)
+    # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
+    if req.get("result").get("action") == "shipping.cost":
+	result = req.get("result")
+	parameters = result.get("parameters")
+	zone = parameters.get("shipping-zone")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+	cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+	speech = "The APPROXIMATE cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
-    print("Response:")
-    print(speech)
+	print("Response:")
+	print(speech)
 
-    return {
-        "speech": speech,
-        "displayText": speech,
-        #"data": {},
-        # "contextOut": [],
-        "source": "apiai-onlinestore-shipping"
-    }
+	return {
+	"speech": speech,
+	"displayText": speech,
+	#"data": {},
+	# "contextOut": [],
+	"source": "apiai-onlinestore-shipping"
+	}
+	
+    else:
+	return {}
+	
 
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
-    print "Starting app on port %d" % port
+    # print "Starting app on port %d" % port
 
     app.run(debug=True, port=port, host='0.0.0.0')
