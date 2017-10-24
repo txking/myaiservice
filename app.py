397d1673c0cfe2215c73dcd8913753383d1057fc
txking <g_rajesh_98@yahoo.com>
2017-10-24 00:55:04 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ import os
 import urllib.request
 import xml.dom.minidom
 import json
+import pprint
 
 from flask import Flask
 from flask import request
 from flask import make_response
+
+from googleapiclient.discovery import build
 
 
 # Flask app should start in global layout
@@ -76,7 +79,32 @@ def makeWebhookResult(req):
         #"data": {},
         # "contextOut": [],
         "source": "rajesh-zoho-creator-loaninfo"
-        }	
+        }
+    elif req.get("result").get("action") == "zoho.getloaninfojson":
+        print("Invoked zoho.getloaninfo")
+        result = req.get("result")
+        parameters = result.get("parameters")
+        loanno = parameters.get("LoanNo")    
+        zohourl = "https://creator.zoho.com/api/json/loaninfo/view/values_Report?authtoken=e22a32c53525cdf5af604978a70a5c4a&zc_ownername=grajesh2000&criteria=(LoanNo%20%3D%3D%20" + loanno + ")"
+        print ('LoanNo is {0}, and URL is {1}'.format(loanno, zohourl))
+        sres = urllib.request.urlopen(zohourl).read()
+        sstr = sres.decode("utf-8")
+        
+        
+        # handle JSON
+        sstr  = sstr[28:-1]
+        speech = json.dumps(sstr, indent=4, sort_keys=True)
+               
+        
+        print("Response:")
+        print(speech)
+        return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "rajesh-zoho-creator-loaninfo"
+        }		
     elif req.get("result").get("action") == "dobuddy.tonecheck":
         print("Invoked dobuddy.tonecheck")
         result = req.get("result")
@@ -89,10 +117,36 @@ def makeWebhookResult(req):
         #"data": {},
         # "contextOut": [],
         "source": "rajesh-watson-tone-check"
+        }
+    elif req.get("result").get("action") == "custom.searchnow":
+        print("Invoked custom.searchnow")
+        result = req.get("result")
+        parameters = result.get("parameters")
+        searchtext = parameters.get("searchtext")    
+        speech = googlesearch(searchtext)
+        return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "rajesh-fnmsite-search"
         }        
     else:
         return {}
-	
+
+
+def googlesearch(stext):
+    print ("GGGGGGGGGG  Will GOOGLE search  GGGGGGGGGGGGGG")
+	print (stext)
+	service = build("customsearch", "v1",developerKey="AIzaSyAIxk6eBuIuSXotmMN2qabwAy5NoLYnk8Y")
+	res = service.cse().list(
+      q='MORA',
+      cx='002730420427000960612:0as1dxnsjnq',
+    ).execute()
+    pprint.pprint(res)
+    return res
+
+		
 def analyze(text):
     anger = list()
     fear = list()
@@ -109,8 +163,8 @@ def analyze(text):
     print (sstr)
     lsret = format_tone_json(sstr)
     return lsret
-     
-        
+
+	
 def format_tone_json(data):
     data = json.loads(str(data))
     print(data)
@@ -123,7 +177,7 @@ def format_tone_json(data):
             lsret = lsret + j['tone_name'].ljust(20) + (str(round(j['score'] * 100,1)) + "%").rjust(10) + '\n'
     print(lsret)
     return lsret
-    
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
